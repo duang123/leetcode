b03bd5432834dfa4fe52f13586ee1bedc82be945
duang123 <643363707@qq.com>
2017-12-31 23:37:13 +0800

    Create 28.cpp

---
+++ 28.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.empty()&& haystack.empty())return 0;
+        if(haystack.empty()||haystack.size()<needle.size())return -1;
+        for(int i=0;i<=haystack.size()-needle.size();i++){
+            int j=0;
+            for(;j!=needle.size();++j){
+                if(needle[j]!=haystack[i+j])break;
+            }
+            if(j==needle.size())return i;
+        }
+        return -1;
+    }
+};
