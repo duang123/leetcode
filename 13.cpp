2b8ff3002342398378e22ef372db30a55d165ad6
duang123 <643363707@qq.com>
2018-10-21 20:11:15 +0800

    Update 13.cpp

--- 13.cpp
+++ 13.cpp
@@ -1,26 +1,21 @@
-#include<unordered_map>
-#include<iostream>
-#include<string>
-using namespace std;
-
-int romanToInt(string s){
-	unordered_map<char,int>m{
-			{'I',1},
-            {'V',5},
-            {'X',10},
-            {'L',50},
-            {'C',100},
-            {'D',500},
-            {'M',1000}
-	};
-	int ret=m[s.back()];
-	for(int i=s.size()-2;i>=0;--i){
-		if(m[s[i]]<m[s[i+1]])ret-=m[s[i]];
-		else ret+=m[s[i]];
-	}
-	return ret;
-}
-int main(){
-	cout<<romanToInt("DCXXI")<<endl;
-	return 0;
-}
+class Solution {
+public:
+    map<char,int>m={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+    int romanToInt(const string& str) {
+        auto n=str.size();
+        int i=0;
+        auto ret=0;
+        
+        for(;i<n;++i){
+            if(i==0)ret+=m[str[i]];
+            if(i){
+                if(m[str[i]]>m[str[i-1]]){
+                    ret=ret-2*m[str[i-1]]+m[str[i]];
+                }else{
+                    ret+=m[str[i]];
+                }
+            }
+        }
+        return ret;
+    }
+};
