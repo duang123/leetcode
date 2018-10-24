213255e879cfade8a94d464a6e83c5227320f05a
duang123 <643363707@qq.com>
2018-10-24 13:12:49 +0800

    Update 26.cpp

--- 26.cpp
+++ 26.cpp
@@ -11,6 +11,29 @@ int removeDuplicates(vector<int>& nums){
 	}
 	return j+1;
 }
+/*
+int removeDuplicates(vector<int>& nums) {
+        if(nums.empty())return 0;
+        auto n=nums.size();
+        int i=0,index=0;
+        auto tmp=0;
+        for(;i<n;++i){
+            if(!i){
+                tmp=nums[i];
+                continue;
+            }
+            if(tmp==nums[i]){
+                continue;
+            }else{
+                tmp=nums[i];
+                nums[++index]=tmp;
+            }
+        }
+        nums.erase(nums.begin()+index+1,nums.end());
+        return index+1;
+    }
+*/
+
 int main(){
 	vector<int>v{1,1,1,12,12,122};
 	cout<<removeDuplicates(v)<<endl;
