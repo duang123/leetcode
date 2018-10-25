031bc2a20bd7452f585411e8904928189ba7fc3a
duang123 <643363707@qq.com>
2018-10-25 13:14:49 +0800

    Update 28.cpp

--- 28.cpp
+++ 28.cpp
@@ -12,4 +12,23 @@ public:
         }
         return -1;
     }
+    
+    /*
+    int strStr(const string& haystack, const string& needle) {
+        if(haystack.empty()&&needle.empty())return 0;
+        if(haystack.empty())return -1;
+        if(needle.empty())return 0;
+        auto m=needle.size(),n=haystack.size();
+        if(m>n)return -1;
+        int i=0;
+        while(i<=n-m){
+            if(haystack.substr(i,m)==needle)
+                    return i;
+             ++i;
+             continue;
+        }
+        return -1;
+    }
+    
+    */
 };
